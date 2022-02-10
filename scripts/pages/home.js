92d21fc039c3d2fe68f65a61334046f50338dcdf
jewelazo <gpalominoe@uni.pe>
2022-02-10 16:15:38 -0500

    code refactor creating pagination component

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -1,63 +1,13 @@
 import Filter from "../components/filter.js"
 import Header from "../components/header.js"
+import Pagination from "../components/pagination.js"
 import { newPrice } from "../helpers/price_dscto.js"
 import STORE from "../store.js"
 
 const Main=(()=>{
     let header = Header()
     let filter = Filter()
-    function onHandlePagination(e){
-        let btnNext=e.target.closest(".next")
-        if (btnNext){
-            STORE.page +=1
-            check()
-            showItems()
-        }
-        let btnPrev=e.target.closest(".prev")
-        if (btnPrev){
-            STORE.page -=1
-            check()
-            showItems()
-        }
-
-    }
-    function showItems(){
-        const galleryItems=document.querySelector(".cards-container").children;
-        const maxItem=6;
-        STORE.maxPage=Math.ceil(galleryItems.length/maxItem);
-        let numberPage=document.querySelector(".page-number")
-        let index=STORE.page;
-        function show() {
-            for(let i=0;i<galleryItems.length; i++){
-                galleryItems[i].classList.remove("show");
-                galleryItems[i].classList.add("hide");
-     
-     
-               if(i>=(index*maxItem)-maxItem && i<index*maxItem){
-               galleryItems[i].classList.remove("hide");
-               galleryItems[i].classList.add("show");
-               }
-            }
-            numberPage.innerHTML=index  
-       }
-       show()
-    }
-    function check(){
-            if(STORE.page===STORE.maxPage || STORE.getAllProducts().length===0){
-                document.querySelector(".next").classList.add("disabled");
-            }
-            else{
-                document.querySelector(".next").classList.remove("disabled");	
-            }
-    
-            if(STORE.page===1){
-                document.querySelector(".prev").classList.add("disabled");
-            }
-            else{
-                document.querySelector(".prev").classList.remove("disabled");	
-            }
-    }
-
+    let pagination = Pagination()
     return {
         render: function (){
             let products=STORE.getAllProducts()
@@ -83,25 +33,17 @@ const Main=(()=>{
                 <div class="cards-container">
                 ${listProducts.length ? listProducts.join(""): "No se encontraron productos"}
                 </div>
-                <div class="pagination">
-                    <div class="prev">Anterior</div>
-                    <div>Página <span class="page-number"></span></div>
-                    <div class="next">Siguiente</div>
-                </div>
+                ${pagination}
                 `
         },
         initEventListeners: function(){
-            
+
             header.addEventListeners()
             filter.addEventListener()
-            showItems()
-            check()
-            const btnPage=document.querySelector(".pagination")
-            if (btnPage){
-                btnPage.addEventListener('click',onHandlePagination)
-            }
-            
-            
+            pagination.addEventListeners()
+            pagination.showItems()
+            pagination.checkPaginate()
+
         }
         
     }
