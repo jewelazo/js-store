83355512204926e2f9cafc375a3c5f44205861b4
jewelazo <gpalominoe@uni.pe>
2022-02-10 00:48:26 -0500

    add loader component

---
+++ scripts/components/loader.js
@@ -0,0 +1,12 @@
+export const Loader=(()=>{
+    return {
+        render: function(){
+            return `
+            <div class="loader"></div>
+            `
+        },
+        initEventListeners: function(){
+            //pass
+        }
+    }
+})()
